8dc3af7b4936ea996eba54b014eb2e3fb5639d31
Ettore Di Giacinto <mudler@sabayon.org>
2020-12-02 22:33:40 +0100

    Place it in /boot

--- packages/kernels/mocaccino/build.sh
+++ packages/kernels/mocaccino/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 MAJOR_VERSION=$(awk -F. '{print $1"."$2}' <<< $PACKAGE_VERSION)
-mkdir output
+mkdir -p output/boot
 pushd linux 
 
 make -j$(nproc --ignore=1)
 
 if [[ -L "arch/${ARCH}/boot/bzImage" ]]; then
-   mv $(readlink -f "arch/${ARCH}/boot/bzImage") ../output/"${KERNEL_PREFIX}-${ARCH}-${MAJOR_VERSION}.0-mocaccino"
+   mv $(readlink -f "arch/${ARCH}/boot/bzImage") ../output/boot/"${KERNEL_PREFIX}-${ARCH}-${MAJOR_VERSION}.0-mocaccino"
 else
-   mv arch/${ARCH}/boot/bzImage ../output/"${KERNEL_PREFIX}-${ARCH}-${MAJOR_VERSION}.0-mocaccino"
+   mv arch/${ARCH}/boot/bzImage ../output/boot/"${KERNEL_PREFIX}-${ARCH}-${MAJOR_VERSION}.0-mocaccino"
 fi
