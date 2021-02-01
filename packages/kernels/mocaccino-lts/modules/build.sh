793529136986b7b5aadbe4c52231dacfb5b34071
Daniele Rondina <geaaru@sabayonlinux.org>
2021-02-01 06:54:04 +0100

    kernel/mocaccino-*: Change naming convention and cleanup
    kernel/mocaccino-sources: Using new naming convention <version>-mocaccino
    
    * drop suffix field. We use env from mocaccino-full
    * we never want sabayon like kernel naming convention without
      PATCH_VERSION.
    
    kernel/mocaccino-modules: Review naming convention
    
    * drop envs inherited by mocaccino-full package
    * add excludes for script and package files
    
    kernel/mocaccino-minimal: Improve includes matches
    
    We use package.version to include files related
    to the handled version.
    
    kernel/mocaccino-full: Add SUFFIX, KERNEL_PREFIX envs
    
    kernel/mocaccino-lts-full: Add SUFFIX, KERNEL_PREFIX envs
    
    kernel/mocaccino-lts-minimal: Improve includes matches
    
    We use package.version to include files related
    to the handled version.
    
    kernel/mocaccino-modules-lts: Review naming convention
    
    * drop envs inherited by mocaccino-full package
    * add excludes for script and package files
    
    kernel/mocaccino-lts-sources: Use new naming convention
    
    * drop suffix field. We use env from mocaccino-full
    * we never want sabayon like kernel naming convention without
      PATCH_VERSION.

--- packages/kernels/mocaccino-lts/modules/build.sh
+++ packages/kernels/mocaccino-lts/modules/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
-MAJOR_VERSION=$(awk -F. '{print $1"."$2}' <<< $PACKAGE_VERSION)
 outdir="/luetbuild/modules"
 mkdir -p $outdir/boot
 pushd linux 
@@ -19,6 +18,9 @@ rm -f "$outdir"/lib/modules/**/build \
 
 popd
 
-mv $outdir/boot/config-$PACKAGE_VERSION $outdir/boot/config-${KERNEL_PREFIX}-${ARCH}-${MAJOR_VERSION}.0-mocaccino
-mv $outdir/boot/System.map-$PACKAGE_VERSION $outdir/boot/System.map-${KERNEL_PREFIX}-${ARCH}-${MAJOR_VERSION}.0-mocaccino
-mv $outdir/boot/vmlinuz-$PACKAGE_VERSION $outdir/boot/vmlinuz-${KERNEL_PREFIX}-${ARCH}-${MAJOR_VERSION}.0-mocaccino
+mv $outdir/boot/config-$PACKAGE_VERSION-${SUFFIX} \
+  $outdir/boot/config-${KERNEL_PREFIX}-${ARCH}-${PACKAGE_VERSION}-${SUFFIX}
+mv $outdir/boot/System.map-$PACKAGE_VERSION-${SUFFIX} \
+  $outdir/boot/System.map-${KERNEL_PREFIX}-${ARCH}-${PACKAGE_VERSION}-${SUFFIX}
+mv $outdir/boot/vmlinuz-$PACKAGE_VERSION-${SUFFIX} \
+  $outdir/boot/vmlinuz-${KERNEL_PREFIX}-${ARCH}-${PACKAGE_VERSION}-${SUFFIX}
